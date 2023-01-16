51ce358f6c02c15a6e333a4910780894fc697e9b
Hez Ronningen <code@hez.ca>
2023-01-16 12:04:09 -0800

    adjust for rc2

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
   {"lib/phoenix/router.ex:2:no_return Function call/2 has no local return."},
-  {"lib/phoenix/router.ex:477:pattern_match The pattern can never match the type :error."}
+  {"lib/phoenix/router.ex:478:pattern_match The pattern can never match the type :error."}
 ]
