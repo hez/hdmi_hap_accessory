11ce5cd19ec72c2335ab69268a0412635be4319a
Hez Ronningen <code@hez.ca>
2023-12-13 13:47:46 -0800

    kill swoosh route, we don't use it

--- lib/hdmi_hap_accessory_web/router.ex
+++ lib/hdmi_hap_accessory_web/router.ex
@@ -8,13 +8,4 @@ defmodule HdmiHapAccessoryWeb.Router do
   scope "/api", HdmiHapAccessoryWeb do
     pipe_through :api
   end
-
-  # Enable Swoosh mailbox preview in development
-  if Application.compile_env(:hdmi_hap_accessory, :dev_routes) do
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
